f9b7e0336fb6d5e70077bb3461d2cec69a1f2d3c
Ai Temmei <h8chi.8@gmail.com>
2024-01-12 23:26:38 +0900

    :wrench: Add: ジェネレータの設定を追加
    ジェネレートコマンド実行時に、CSS,
    JSファイルとヘルパーファイルを作成しないよう設定

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,10 @@ module WalkOnAlleyways
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    # generatorを設定
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
