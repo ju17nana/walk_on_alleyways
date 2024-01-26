cdbea332e9ce69c788fb2d7527beb88f11de5e20
Ai Temmei <h8chi.8@gmail.com>
2024-01-26 15:33:14 +0900

    :wrench: Add: RSpecでテストを実行できるよう設定
    RSpecの出力形式をDocumantationにし、ジェネレータを設定

--- config/application.rb
+++ config/application.rb
@@ -48,6 +48,11 @@ module WalkOnAlleyways
     config.generators do |g|
       g.assets false
       g.helper false
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        request_specs: false
     end
   end
 end
