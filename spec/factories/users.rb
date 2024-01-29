56b848064b76e82098bb9cb5acaa66affde433c8
Ai Temmei <h8chi.8@gmail.com>
2024-01-29 12:00:06 +0900

    :sparkles: Add: Userモデルのバリデーションに関するスペックを追加
    FactoryBotによるテスト用ユーザーデータの生成と、バリデーションに関するテストを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:email) { |n| 'test_#{n}@example.com' }
+    password { 'password' }
+    #password_confirmation { 'password' }
+
+    # emailが'test@example.com'
+    trait :fixed_email do
+      email { 'test@example.com' }
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'email, crypted_password, saltがあれば有効な状態であること' do
+    user = FactoryBot.build(:user)
+    expect(user).to be_valid
+  end
+
+  it 'emailがなければ無効な状態であること' do
+    user = FactoryBot.build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include('メールアドレスを入力してください')
+  end
+
+  it 'passwordがなければ無効な状態であること' do
+    user = FactoryBot.build(:user, password: nil)
+    user.valid?
+    expect(user.errors[:password]).to include('パスワードを入力してください')
+  end
+
+  it 'emailが重複していれば無効な状態であること' do
+    FactoryBot.create(:user, :fixed_email)
+    user = FactoryBot.build(:user, :fixed_email)
+    user.valid?
+    expect(user.errors[:email]).to include('入力したメールアドレスはすでに登録されています')
+  end
 end
