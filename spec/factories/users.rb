c61523906c8344d795c9368f8ea2b4c032027e36
Ai Temmei <h8chi.8@gmail.com>
2024-01-30 09:45:05 +0900

    :rotating_light: Fix: Userモデルのモデルスペックを修正
    バリデーションメッセージに関するテストを一旦コメントアウトし、Passwordの長さに関するスペックを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,11 +2,29 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| 'test_#{n}@example.com' }
     password { 'password' }
-    #password_confirmation { 'password' }
+    password_confirmation { 'password' }
 
     # emailが'test@example.com'
     trait :fixed_email do
       email { 'test@example.com' }
+    end
+
+    # passwordが7文字
+    trait :too_short_password do
+      password { 'a' * 7 }
+      password_confirmation { 'a' * 7 }
+    end
+
+    # passwordが32文字
+    trait :long_password do
+      password { 'a' * 32 }
+      password_confirmation { 'a' * 32 }
+    end
+
+    # passwordが33文字
+    trait :too_long_password do
+      password { 'a' * 33 }
+      password_confirmation { 'a' * 33 }
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,27 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'email, crypted_password, saltがあれば有効な状態であること' do
+  it 'email, passwordがあれば有効な状態であること' do
     user = FactoryBot.build(:user)
+    expect(user).to be_valid
+  end
+
+  it 'passwordが32文字であれば有効な状態であること' do
+    user = FactoryBot.build(:user, :long_password)
     expect(user).to be_valid
   end
 
   it 'emailがなければ無効な状態であること' do
     user = FactoryBot.build(:user, email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include('メールアドレスを入力してください')
+#    user.valid?
+#    expect(user.errors[:email]).to include('メールアドレスを入力してください')
+    expect(user).to_not be_valid
   end
 
   it 'passwordがなければ無効な状態であること' do
     user = FactoryBot.build(:user, password: nil)
-    user.valid?
-    expect(user.errors[:password]).to include('パスワードを入力してください')
+#    user.valid?
+#    expect(user.errors[:password]).to include('パスワードを入力してください')
+    expect(user).to_not be_valid
+  end
+
+  it 'passwordが8文字未満であれば無効な状態であること' do
+    user = FactoryBot.build(:user, :too_short_password)
+#    user.valid?
+#    expect(user.errors[:password]).to include('パスワードは8文字以上で入力してください')
+    expect(user).to_not be_valid
+  end
+
+  it 'passwordが33文字以上であれば無効な状態であること' do
+    user = FactoryBot.build(:user, :too_long_password)
+#    user.valid?
+#    expect(user.errors[:password]).to include('パスワードは32文字以内で入力してください')
+    expect(user).to_not be_valid
   end
 
   it 'emailが重複していれば無効な状態であること' do
     FactoryBot.create(:user, :fixed_email)
     user = FactoryBot.build(:user, :fixed_email)
-    user.valid?
-    expect(user.errors[:email]).to include('入力したメールアドレスはすでに登録されています')
+#    user.valid?
+#    expect(user.errors[:email]).to include('入力したメールアドレスはすでに登録されています')
+    expect(user).to_not be_valid
   end
 end
