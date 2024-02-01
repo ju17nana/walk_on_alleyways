7d226b9ab2797ff106b268a4e026a172b7e53a3a
Ai Temmei <h8chi.8@gmail.com>
2024-02-01 14:07:14 +0900

    :construction: WIP: ログイン機能を追加
    Sorceryを利用したログイン機能を追加。スタイル・ログイン後のヘッダーフッターはまだ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
   root to: 'static_pages#top'
-
   resources :users, only: %i[ new create destroy ]
+
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
