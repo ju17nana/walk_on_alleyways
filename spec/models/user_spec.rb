b415374b5f2cbc27674b87ce9eeb67271bbda673
Ai Temmei <h8chi.8@gmail.com>
2024-02-03 23:21:56 +0900

    :rotating_light: Fix: Userモデルのモデルスペックのバリデーションエラーメッセージについてのテストを有効化
    コメントアウトしていたバリデーションメッセージに関するテストを修正し、有効化

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,44 +13,44 @@ RSpec.describe User, type: :model do
 
   it 'emailがなければ無効な状態であること' do
     user = build(:user, email: nil)
-#    user.valid?
-#    expect(user.errors[:email]).to include('メールアドレスを入力してください')
-    expect(user).not_to be_valid
+    user.valid?
+    expect(user.errors[:email]).to include('を入力してください')
+#    expect(user).not_to be_valid
   end
 
   it 'passwordがなければ無効な状態であること' do
     user = build(:user, password: nil)
-#    user.valid?
-#    expect(user.errors[:password]).to include('パスワードを入力してください')
-    expect(user).not_to be_valid
+    user.valid?
+    expect(user.errors[:password]).to include('は8文字以上で入力してください')
+#    expect(user).not_to be_valid
   end
 
-  it 'passwordとpassword(確認)が一致しなければ無効な状態であること' do
+  it 'password(確認)がなければ無効な状態であること' do
     user = build(:user, password_confirmation: nil)
-#    user.valid?
-#    expect(user.errors[:password]).to include('パスワードとパスワード(確認)が一致しません。もう一度入力してください')
-    expect(user).not_to be_valid
+    user.valid?
+    expect(user.errors[:password_confirmation]).to include('を入力してください')
+#    expect(user).not_to be_valid
   end
 
   it 'passwordが8文字未満であれば無効な状態であること' do
     user = build(:user, password: ('a' * 7), password_confirmation: ('a' * 7))
-#    user.valid?
-#    expect(user.errors[:password]).to include('パスワードは8〜32文字で入力してください')
-    expect(user).not_to be_valid
+    user.valid?
+    expect(user.errors[:password]).to include('は8文字以上で入力してください')
+#    expect(user).not_to be_valid
   end
 
   it 'passwordが33文字以上であれば無効な状態であること' do
     user = build(:user, password: ('a' * 33), password_confirmation: ('a' * 33))
-#    user.valid?
-#    expect(user.errors[:password]).to include('パスワードは8〜32文字以内で入力してください')
-    expect(user).not_to be_valid
+    user.valid?
+    expect(user.errors[:password]).to include('は32文字以内で入力してください')
+#    expect(user).not_to be_valid
   end
 
   it 'emailが重複していれば無効な状態であること' do
     create(:user, :fixed_email)
     user = build(:user, :fixed_email)
-#    user.valid?
-#    expect(user.errors[:email]).to include('入力したメールアドレスはすでに登録されています')
-    expect(user).not_to be_valid
+    user.valid?
+    expect(user.errors[:email]).to include('はすでに存在します')
+#    expect(user).not_to be_valid
   end
 end
