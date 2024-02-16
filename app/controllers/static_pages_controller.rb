4c1b02cd1195795fe155b4845211faa8a6f7804a
Ai Temmei <h8chi.8@gmail.com>
2024-02-17 00:02:43 +0900

    :sparkles: Change: 未ログイン状態の時、ヘッダーロゴの遷移先をAboutページに変更
    ヘッダーロゴの遷移先を変更し、Topアクションに未ログイン状態の場合Aboutページにリダイレクトするよう設定

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: [:top , :about]
-  def top; end
+  skip_before_action :require_login, only: [:top, :about]
+  def top
+    redirect_to about_path unless logged_in?
+  end
 
   def about; end
 end
