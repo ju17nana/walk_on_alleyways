8de5b03a720b280c27f5007a13235a04cdb71156
Ai Temmei <h8chi.8@gmail.com>
2024-02-18 01:37:47 +0900

    :recycle: Fix: Lintチェック・修正
    Lintチェックの結果に基づいて修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: [:top, :about]
+  skip_before_action :require_login, only: %i[top about]
   def top
     redirect_to about_path unless logged_in?
   end
